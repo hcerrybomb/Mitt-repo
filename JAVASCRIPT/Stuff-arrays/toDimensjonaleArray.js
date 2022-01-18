83208e976b8d6acd9174658ecad4d8e0d438ebd2
SiegeMann <william.strzelecki.04@gmail.com>
2022-01-18 09:53:27 +0100

    JS objekter + reorganization

--- JAVASCRIPT/Stuff-arrays/toDimensjonaleArray.js
+++ JAVASCRIPT/Stuff-arrays/toDimensjonaleArray.js
@@ -13,8 +13,33 @@ for(let i = 0; i < opg1List.length; i++){
         console.log("1. D)  ",opg1List[i][x])
     }
 }
+for(let i = 0; i < opg1List.length; i++){
+    x = 0
+    console.log("1. E)",opg1List[i][x],opg1List[i][x+1],opg1List[i][x+2])
+}
 
-//TODO      GJØR OPPGAVE E
+//!     OPPGAVE 2
 
-//!         OPPGAVE 2 
+const opg2List = []
+for(let i = 1; i < 11; i++){opg2List.push([i])}
+for (let x = 0; x < 10; x++) {for (let y = 1; y < 11; y++){opg2List[x].push((x+1)*y)}}
+console.log('2.');console.table(opg2List)
 
+//!     OPPGAVE 3
+
+const opg3List = [['audi',    'rød',  'AK 12345'],['opel',    'blå',  'BG 97345'],['mercedes','sølv', 'GH 10835']]
+console.table(opg3List)
+//*let nyBilMerke = prompt('Please enter car label')
+let nyBilMerke = 'Aston Martin'
+//*let nyBilFarge = prompt('Please enter car color')
+let nyBilFarge = 'grå'
+//*let nyBilNummer = prompt('Please enter car license plate')
+let nyBilNummer = 'UD 18567'
+let alreadyIn = false
+for (let i = 0; i < opg3List.length; i++) {
+if(opg3List[i].indexOf(nyBilNummer) !== -1){alreadyIn = true}}
+if(!alreadyIn){
+    opg3List.push([nyBilMerke,nyBilFarge,nyBilNummer])
+    console.log('3. a + b)')
+    console.table(opg3List)}
+else{console.log('plate already registered')}
