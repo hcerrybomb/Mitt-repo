74cd8f30ce7d2c037f0da653e622b6032ab2e20d
SiegeMann <william.strzelecki.04@gmail.com>
2022-01-25 18:06:56 +0100

    asd

--- PYTHON/RustAutomated/antiafk.py
+++ PYTHON/RustAutomated/antiafk.py
@@ -1,7 +1,13 @@
 from tkinter import E
-from pynput.keyboard import *
+from pynput.keyboard import * 
 from time import time, ctime, sleep
 keyboard = Controller()
 while 1>0:
-    keyboard.press('e')
+    keyboard.press('c')
     sleep(1)
+    keyboard.release('c')
+    sleep(1)
+    keyboard.press(Key.space)
+    sleep(1)
+    keyboard.release(Key.space)
+    sleep(2000)
