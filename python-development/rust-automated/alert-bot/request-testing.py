00fe0820e1c3bee97f0ca2e1b4d6e8b440ea705b
SiegeMann <william.strzelecki.04@gmail.com>
2022-03-08 20:15:38 +0100

    Merge branch 'main' of https://github.com/SiegeMann/Onedrive-skole

--- python-development/rust-automated/alert-bot/request-testing.py
+++ python-development/rust-automated/alert-bot/request-testing.py
@@ -2,6 +2,7 @@ import requests
 import json
 #x = requests.post(newURL)
 #newURL = "https://api.battlemetrics.com/players/match?data[identifier]='76561198313072306'&data[type]='steamID'&data='identifier'&access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImQ2NzY0ZjU3OTcwY2ZlYmYiLCJpYXQiOjE2NDY1NjU2NTUsIm5iZiI6MTY0NjU2NTY1NSwiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjo1Mjk3NTMifQ.CCAsM9QodXjl1OUi-dqY-PFnv5_2_d6J8eShuNuJ9oQ"
+newURL = "https://api.battlemetrics.com/players/match?data[identifier]='76561198313072306'&data[type]='steamID'&data='identifier'&access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImQ2NzY0ZjU3OTcwY2ZlYmYiLCJpYXQiOjE2NDY1NjU2NTUsIm5iZiI6MTY0NjU2NTY1NSwiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjo1Mjk3NTMifQ.CCAsM9QodXjl1OUi-dqY-PFnv5_2_d6J8eShuNuJ9oQ"
 
 token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImQ2NzY0ZjU3OTcwY2ZlYmYiLCJpYXQiOjE2NDY1NjU2NTUsIm5iZiI6MTY0NjU2NTY1NSwiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjo1Mjk3NTMifQ.CCAsM9QodXjl1OUi-dqY-PFnv5_2_d6J8eShuNuJ9oQ'
 
@@ -15,16 +16,44 @@ PARAMS = {
     'data':'identifier'
 }
 
+DATA = {
+  "data": [
+    {
+      "type": "identifier",
+      "attributes": {
+        "type": "steamID",
+        "identifier": "76561198171172242"
+      }
+    }
+  ]
+}
+
+#DATA = [{
+#   'url':'/players/match',
+#    'params':{
+#        'data[identifier]':identifier,
+#        'data[type]':'steamID',
+#        'data':'identifier' 
+#    },
+#    'method':'post'
+#}]
+
+
+
 HEADERS = {
     'Content-Type':'application/json',
     'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImQ2NzY0ZjU3OTcwY2ZlYmYiLCJpYXQiOjE2NDY1NjU2NTUsIm5iZiI6MTY0NjU2NTY1NSwiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjo1Mjk3NTMifQ.CCAsM9QodXjl1OUi-dqY-PFnv5_2_d6J8eShuNuJ9oQ'
 }
 
 x = requests.post(url=URL, params=PARAMS, headers=HEADERS)
+#x = requests.post(newURL)
 
+x = requests.post(url=URL,data=json.dumps(DATA),headers=HEADERS)
+print(x.url)
 data = x.json()
 
 parsed_data = json.dumps(data,indent=4,sort_keys=True)
 
 print(parsed_data)
 
+print(parsed_data)
--- python-development/rust-automated/alert-bot/some-testing.py
+++ python-development/rust-automated/alert-bot/some-testing.py
@@ -1,19 +1,77 @@
 import requests
+import json 
+from datetime import datetime
+
+now = datetime.now()
+now = str(now)
+now = list(now)
+now[10] = "T"; now[19] = "Z"
+now = now[0:20]
+
+month = now[5] + now[6]
+
+
+month = int(month)
+print(month)
+
+
+
+now = "".join(now)
+print(now)
+
+
+
 
 URL = "https://api.battlemetrics.com/servers"
 
+URL2 = "https://api.battlemetrics.com/players"
+
 searchTerm = "Rustopia EU Main"
 
-PARAMS = {"filter[search]":searchTerm,"filter[game]":"rust"}
+playerSearchTerm = "[SB] ostritch"
 
+PARAMS = {
+    "filter[search]":searchTerm,
+    "filter[game]":"rust",
+    }
 
-
+playerPARAMS = {
+    "filter[search]":playerSearchTerm,
+    "filter[after]":now
+}
 
 x = requests.get(url = URL, params = PARAMS)
+y = requests.get(url = URL2, params = playerPARAMS)
+
+data2 = y.json()
 
 data = x.json()
 
-print(data)
+parsed_data2 = json.dumps(data2,indent=4,sort_keys=True)
+parsed_data = json.dumps(data,indent=4,sort_keys=True)
 
-print(x)
+#print(parsed_data2)
+
+parsed_data_dict = json.loads(parsed_data)
+parsed_data_dict2 = json.loads(parsed_data2)
+
+#print(len(parsed_data_dict["data"]))
+if False:
+    for i in range(len(parsed_data_dict["data"])):
+        print(parsed_data_dict["data"][i]["attributes"]["ip"])
+        print(parsed_data_dict["data"][i]["attributes"]["name"])
+        print(parsed_data_dict["data"][i]["attributes"]["players"])
+        print(parsed_data_dict["data"][i]["attributes"]["status"])
+        print(parsed_data_dict["data"][i]["id"])
+
+
+
+with open("C:\\Users\\wista002\\Desktop\\Mitt-repo\\python-development\\env-new-environment\\request.json","w") as outfile:
+#with open("C:\\Users\\Gaming_Dator_VII\Desktop\\Mitt-repo\\python-development\\env-new-environment\\request.json","w") as outfile:
+
+    outfile.write(parsed_data2)
+    print()
+
+
+    
 
