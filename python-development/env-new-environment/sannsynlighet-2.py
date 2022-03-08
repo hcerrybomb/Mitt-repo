c6fa29ad86cf5caee46ac0cdf2c6d4cb5cd3035f
SiegeMann <william.strzelecki.04@gmail.com>
2022-03-08 19:36:08 +0100

    Rust Alert Bot

--- python-development/env-new-environment/sannsynlighet-2.py
+++ python-development/env-new-environment/sannsynlighet-2.py
@@ -1,13 +1,19 @@
-#%%
+curl -n -X POST https://api.battlemetrics.com/players/match \ -d '{"data": [{"type": "identifier","attributes": {"type": "steamID","identifier": "76561198171172242"}}]}' \ -H "Content-Type: application/json" \ -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImQ2NzY0ZjU3OTcwY2ZlYmYiLCJpYXQiOjE2NDY1NjU2NTUsIm5iZiI6MTY0NjU2NTY1NSwiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjo1Mjk3NTMifQ.CCAsM9QodXjl1OUi-dqY-PFnv5_2_d6J8eShuNuJ9oQ"
 
-from pylab import *
-utfallsrom = [1,2,3,4,5,6]
-f=0
-for i in range(1,600):
-    k=choice(utfallsrom)
-    #print(k)
-    if k==6:
-        f+=1
-print(f"frekvens seksere {f}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # %%
