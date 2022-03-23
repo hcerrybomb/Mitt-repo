a433b9b941d31d33433420f78b46f27eb83ab948
SiegeMann <william.strzelecki.04@gmail.com>
2022-03-23 02:28:03 +0100

    Alert Bot

--- python-development/rust-automated/focus-window.py
+++ python-development/rust-automated/focus-window.py
@@ -34,39 +34,39 @@ print(correctProgram)
 handle = win32gui.FindWindow(None, correctProgram)  
 if x==x:
 
-        print("1does it focus test", str(handle), correctProgram)
-        win32gui.BringWindowToTop(handle)
-        time.sleep(0.5)
-        print("2does it focus test", str(handle), correctProgram)  
-        win32gui.SetForegroundWindow(handle)
-        time.sleep(0.5)
+    print("1does it focus test", str(handle), correctProgram)
+    win32gui.BringWindowToTop(handle)
+    time.sleep(0.5)
+    print("2does it focus test", str(handle), correctProgram)  
+    win32gui.SetForegroundWindow(handle)
+    time.sleep(0.5)
 
-        win32gui.SetActiveWindow(handle)
-
-
+    win32gui.SetActiveWindow(handle)
 
 
 
-        remote_thread, _ = wproc.GetWindowThreadProcessId(handle)
-        wproc.AttachThreadInput(wapi.GetCurrentThreadId(), remote_thread, True)
 
-        print("3does it focus test", str(handle), correctProgram)
 
-        win32gui.SetFocus(handle)
-        time.sleep(0.5)
-        print("4does it focus test", str(handle), correctProgram)
-        win32gui.ShowWindow(handle, win32con.SW_MAXIMIZE)
-        time.sleep(0.5)
-        print("5does it focus test", str(handle), correctProgram)
-        win32gui.BringWindowToTop(handle)
-        time.sleep(0.5)
-        print("6does it focus test", str(handle), correctProgram)  
-        win32gui.SetForegroundWindow(handle)
-        time.sleep(0.5)
-        print("7does it focus test", str(handle), correctProgram)
-        win32gui.SetFocus(handle)
-        time.sleep(0.5)
-        print("8does it focus test", str(handle), correctProgram)
+    remote_thread, _ = wproc.GetWindowThreadProcessId(handle)
+    wproc.AttachThreadInput(wapi.GetCurrentThreadId(), remote_thread, True)
+
+    print("3does it focus test", str(handle), correctProgram)
+
+    win32gui.SetFocus(handle)
+    time.sleep(0.5)
+    print("4does it focus test", str(handle), correctProgram)
+    win32gui.ShowWindow(handle, win32con.SW_MAXIMIZE)
+    time.sleep(0.5)
+    print("5does it focus test", str(handle), correctProgram)
+    win32gui.BringWindowToTop(handle)
+    time.sleep(0.5)
+    print("6does it focus test", str(handle), correctProgram)  
+    win32gui.SetForegroundWindow(handle)
+    time.sleep(0.5)
+    print("7does it focus test", str(handle), correctProgram)
+    win32gui.SetFocus(handle)
+    time.sleep(0.5)
+    print("8does it focus test", str(handle), correctProgram)
 
 
 print("program continuing")
